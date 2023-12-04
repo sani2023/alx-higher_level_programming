5bb5d759a3a6c93070b484e26a62745ecf1d5f16
sani2023 <hassanabdousani2023@gmail.com>
2023-12-03 18:43:29 -0800

    101

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,14 +8,18 @@ if __name__ == "__main__":
         print('Usage: {} <a> <operator> <b>'.format(argv[0]))
         exit(1)
 
-        ops = {'+': add, '-': sub, '*': mul, '/': div}
-
+        ops = {
+                '+': add,
+                '-': sub,
+                '*': mul,
+                '/': div
+                }
         if argv[2] in ops:
-            num1 = int(argv[1])
-            num2 = int(argv[3])
-            op = ops[argv[2]]
-            result = op(num1, num2)
-            print('{:d} {:d} = {:d}'.format(num1, argv[2], num2, result))
+            a = int(argc[1])
+            b = int(argc[3])
+            op = ops[argc[2]]
+            result = ops(a, b)
+            print('{:d} {:s} {:d} = {:d}'.format(a, argv[2], b, result))
         else:
             print('Unknown operator. Avaible operators: +, -, *, and /')
             exit(1)
